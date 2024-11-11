9cbff52e26ed59a24deda9b036bf3eb8ed859d6d
moonclash <emladenoff@gmail.com>
2024-11-11 06:48:44 +0200

    refactor setup to use webhooks in stead of polling

--- src/strava.py
+++ src/strava.py
@@ -1,12 +1,13 @@
 import os
 import requests
-from datetime import datetime, timedelta
+from datetime import timedelta
 from storage import DbManager
-from util import string_to_date, generate_activity_name
+from util import string_to_date, generate_activity_name, refresh_strava_token, refresh_spotify_token
 
 GET_ACTIVITIES_URL = "https://www.strava.com/api/v3/athlete/activities"
 GET_TOKEN_URL = "https://www.strava.com/oauth/token"
 UPDATE_ACTIVITY_URL = "https://www.strava.com/api/v3/activities"
+GET_ACTIVITY_URL = "https://www.strava.com/api/v3/activities"
 
 CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
 CLIENT_SECRET = os.getenv("STRAVA_SECRET")
@@ -33,55 +34,23 @@ def get_token():
     )
 
 
-def refresh_token():
-    response = requests.post(
-        url=GET_TOKEN_URL,
-        params={
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
-            "grant_type": "refresh_token",
-            "refresh_token": DbManager.get_token().get("strava_refresh_token"),
-        },
-    )
-    response_data = response.json()
-    DbManager.update_token(
-        {
-            "strava_access_token": response_data.get("access_token"),
-            "strava_refresh_token": response_data.get("refresh_token"),
-        }
-    )
-    return response_data
-
-
-def get_recent_activities():
-    now = datetime.now()
-    midnight = now.replace(
-        hour=0, minute=0, second=0, microsecond=0
-    )
-    midnight_timestamp = round(midnight.timestamp())
+def get_single_activity(activity_id):
     response = requests.get(
-        url=GET_ACTIVITIES_URL,
+        url=f"{GET_ACTIVITY_URL}/{activity_id}",
         headers={
             "Authorization": f"Bearer {DbManager.get_token().get('strava_access_token')}"
         },
-        params={"after": midnight_timestamp},
     )
     response_data = response.json()
-    activities = []
-    print(response_data)
-    for activity in response_data:
-        activities.append(
-            {
-                "activity_id": activity.get("id"),
-                "after": activity.get("start_date"),
-                "type": activity.get("type"),
+    return {
+                "activity_id": response_data.get("id"),
+                "after": response_data.get("start_date"),
+                "type": response_data.get("type"),
                 "before": (
-                    string_to_date(activity.get("start_date"))
-                    + timedelta(seconds=activity.get("elapsed_time"))
+                    string_to_date(response_data.get("start_date"))
+                    + timedelta(seconds=response_data.get("elapsed_time"))
                 ).strftime("%Y-%m-%dT%H:%M"),
             }
-        )
-    return activities
 
 
 def update_activity_description(activity_id, description, name=None):
@@ -98,21 +67,22 @@ def update_activity_description(activity_id, description, name=None):
     return response
 
 
-def update_activities_with_songs():
-    activities = get_recent_activities()
-    for activity in activities:
-        songs_response = requests.get(
-            f"http://{HOST}:8000/recently-played",
-            params={
-                "after": activity.get("after"),
-                "before": activity.get("before"),
-                "limit": 50,
-            },
-        )
-        songs_response_data = songs_response.json().get("songs")
-        activity_name = generate_activity_name(activity.get("type"), songs_response_data)
-        update_activity_description(
-            activity.get("activity_id"),
-            description="🎵 Songs played 🎵: \n" + "\n".join(songs_response_data),
-            name=activity_name,
-        )
+def update_activity_with_songs(activity_id):
+    refresh_strava_token()
+    refresh_spotify_token()
+    activity = get_single_activity(activity_id)
+    songs_response = requests.get(
+        f"http://{HOST}:8000/recently-played",
+        params={
+            "after": activity.get("after"),
+            "before": activity.get("before"),
+            "limit": 50,
+        },
+    )
+    songs_response_data = songs_response.json().get("songs")
+    activity_name = generate_activity_name(activity.get("type"), songs_response_data)
+    update_activity_description(
+        activity.get("activity_id"),
+        description="🎵 Songs played 🎵: \n" + "\n".join(songs_response_data),
+        name=activity_name,
+    )
